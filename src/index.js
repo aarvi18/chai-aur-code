7edc7e1aa1c4be758dd4dc3dbbd26a3fce5d4d09
Rakesh <rakeshcse123@gmail.com>
2023-11-15 19:16:31 -0800

    Change in app.js file

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection faild!!",err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express();
+
+// ( async () => {
+//     try {
+//         mongoose.connect(`${process.env.MONDODB_URI}${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("Error: ",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listining on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("Error",error)
+//         throw error
+//     }
+// })()
